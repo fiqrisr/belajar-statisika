90bf28e3992316423d71c15968be381a66a7e897
Fiqri Syah Redha <wibulo27@gmail.com>
2021-06-12 05:37:53 +0700

    optimize fonts

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 module.exports = {
 	entry: path.resolve(__dirname, 'src/scripts/index.js'),
@@ -24,8 +26,9 @@ module.exports = {
 				],
 			},
 			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
 				type: 'asset/resource',
+				include: [path.resolve(__dirname, 'src/fonts')],
 				generator: {
 					filename: 'assets/fonts/[contenthash:8][ext]',
 				},
@@ -35,6 +38,15 @@ module.exports = {
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: '[name].css',
+		}),
+		new WebpackShellPluginNext({
+			onBuildEnd: {
+				scripts: [
+					() => {
+						fs.unlinkSync(path.resolve(__dirname, '_site/main.js'));
+					},
+				],
+			},
 		}),
 	],
 };
